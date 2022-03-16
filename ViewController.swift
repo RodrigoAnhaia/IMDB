f5136a3f2b2c6d78e5ca511fc2121dd2caae65a7
RodrigoS.A <rodrigoanhaia2@hotmail.com>
2022-03-15 22:37:09 -0300

    First commit
    clean project

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDB
+//
+//  Created by Rodrigo de Anhaia on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
